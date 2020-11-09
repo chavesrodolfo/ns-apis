cb5c73d15716f718c2cb79db7daa8f0ed6c3bcf2
Rodolfo <chavesrodolfo@gmail.com>
2020-11-09 09:32:09 -0300

    adjustments

--- src/main/java/com/gituhub/chavesrodolfo/campaign/model/Campaign.java
+++ src/main/java/com/gituhub/chavesrodolfo/campaign/model/Campaign.java
@@ -16,14 +16,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Campaign {
     
-    public Campaign(CampaignRepresentation campaignRepresentation) {
-        this.id = campaignRepresentation.getId();
-        this.name = campaignRepresentation.getName();
-        this.startDate = campaignRepresentation.getStartDate();
-        this.endDate = campaignRepresentation.getEndDate();
-        this.heartClub = campaignRepresentation.getHeartClub();
-    }
-    
 	@Id
 	@GeneratedValue
     private Long id;
@@ -33,13 +25,21 @@ public class Campaign {
     private Date endDate;
     private Long heartClub;
 
+    public Campaign(CampaignRepresentation campaignRepresentation) {
+        this.id = campaignRepresentation.getId();
+        this.name = campaignRepresentation.getName();
+        this.startDate = campaignRepresentation.getStartDate();
+        this.endDate = campaignRepresentation.getEndDate();
+        this.heartClub = campaignRepresentation.getHeartClub();
+    }
+
 	public CampaignRepresentation transformToRepresentation() {
         CampaignRepresentation campaignRepresentation = new CampaignRepresentation();
-        campaignRepresentation.setName(this.getName());
-        campaignRepresentation.setHeartClub(this.getHeartClub());
+        campaignRepresentation.setName(this.name);
+        campaignRepresentation.setHeartClub(this.heartClub);
         campaignRepresentation.setStartDate(this.startDate);
         campaignRepresentation.setEndDate(this.endDate);
-        campaignRepresentation.setId(this.getId());
+        campaignRepresentation.setId(this.id);
 		return campaignRepresentation;
     }
     
